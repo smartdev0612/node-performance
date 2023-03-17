6b474efde02bb5e832713bad8574c9167fc8854c
Smart Dev <danieldev0612@gmail.com>
2023-03-17 16:10:33 -0700

    update optimizing performance using pm2

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express')
-const cluster = require('cluster')
-const os = require('os')
 
 const app = express()
 
@@ -19,19 +17,10 @@ app.get('/', (req, res) => {
 })
 
 app.get('/timer', (req, res) => {
-  delay(9000)
-  res.send(`Ding ding ding! ${process.pid}`)
+  delay(4000)
+  res.send(`Beep beep beep! ${process.pid}`)
 })
 
 console.log('Running server.js...')
-
-if (cluster.isMaster) {
-  console.log('Master has been started...')
-  const NUM_WORKERS = os.cpus().length
-  for (let i = 0; i < NUM_WORKERS; i++) {
-    cluster.fork()
-  }
-} else {
-  console.log('Worker process started')
-  app.listen(3000)
-}
+console.log('Worker process started')
+app.listen(3000)
